b97dfa510ce1839990508c3c37fd632e810bef15
kkanchamreddy <kkanchamreddy@ebay.com>
2013-07-16 17:44:15 -0700

    test again

---
+++ ArrayDemo.java
@@ -0,0 +1,25 @@
+public class ArrayDemo {
+    public static void main(String[] args) {
+	int[] anArray;	        // DECLARE an array of integers
+
+	anArray = new int[10];	// CREATE an array of integers
+     
+	 String aString = "bob";
+	 String b="pop";
+     //b.replace('b', 'p');
+     if(b.equals("pop")){
+        System.out.println("true");
+	 }
+
+	// assign a value to each array element 
+	for (int i = 0; i < anArray.length; i++) {
+		anArray[i] = i;
+	    }
+
+	// print a value from each array element
+	for (int i = 0; i < anArray.length; i++) {
+	    System.out.print(anArray[i] + " ");
+	}
+	System.out.println();
+    }
+}
--- test.js
+++ test.js
@@ -1,10 +1,21 @@
-function test() {
-	"use strict";
-	var someVar = 10;
-	if (someVar < 12) {
-		someVar = someVar + 1;
-	}
-	if (someVar < 13){
-		someVar = someVar + 1;
-	}
-}
+(function( undefined ) {
+    
+ 
+    function test1( arg1, arg2, arg3, arg4 ) {
+        console.log( 'too many parameters!' );
+        if ( arg1 === 1 ) {
+            console.log( arg1 );
+            if ( arg2 === 2 ) {
+                console.log( arg2 );
+                if( arg3 === 3 ) {
+                    console.log( 'too much nesting!' );
+                    console.log( arg3 );
+                    console.log( arg4 );
+                }
+            }
+        }
+        console.log( 'too many statements!' );
+    }
+ 
+    test1( 1, 2, 3, 4 );
+}());
